3517743092a3a8d9bbb8042b1d556090c47c92e8
David Egbuwalo <lereegbuwalo@gmail.com>
2025-08-09 15:47:52 +0100

    first storybook component

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,72 @@
+
+import {Component, input, output} from '@angular/core';
+import {NgIf} from "@angular/common";
+
+@Component({
+    selector: 'app-task',
+    standalone: true,
+    template: `
+		<div class="list-item {{ task()?.state }}">
+			<label
+				[attr.aria-label]="'archiveTask-' + task()?.id"
+				for="checked-{{ task()?.id }}"
+				class="checkbox"
+			>
+				<input
+					type="checkbox"
+					disabled="true"
+					[defaultChecked]="task()?.state === 'TASK_ARCHIVED'"
+					name="checked-{{ task()?.id }}"
+					id="checked-{{ task()?.id }}"
+				/>
+				<span class="checkbox-custom" (click)="onArchive(task()?.id)"></span>
+			</label>
+			<label
+				[attr.aria-label]="task()?.title + ''"
+				for="title-{{ task()?.id }}"
+				class="title"
+			>
+				<input
+					type="text"
+					[value]="task()?.title"
+					readonly="true"
+					id="title-{{ task()?.id }}"
+					name="title-{{ task()?.id }}"
+					placeholder="Input title"
+				/>
+			</label>
+            @if(task()?.state !== 'TASK_ARCHIVED') {
+                <button
+                    class="pin-button"
+                    [attr.aria-label]="'pinTask-' + task()?.id"
+                    (click)="onPin(task()?.id)"
+                >
+                    <span class="icon-star"></span>
+                </button>
+			}
+		</div>
+    `,
+    imports: [
+        NgIf
+    ]
+})
+export default class TaskComponent {
+    /**
+     * The shape of the task object
+     */
+    task = input<any>()
+
+    // tslint:disable-next-line: no-output-on-prefix
+    onPinTask = output<Event>()
+
+    // tslint:disable-next-line: no-output-on-prefix
+    onArchiveTask = output<Event>();
+
+    onPin(id: any) {
+        this.onPinTask.emit(id);
+    }
+
+    onArchive(id: any) {
+        this.onArchiveTask.emit(id);
+    }
+}
---
+++ src/app/task/task.stories.ts
@@ -0,0 +1,36 @@
+import {fn} from "@storybook/test";
+import TaskComponent from "./task.component";
+import {Meta, StoryObj} from "@storybook/angular";
+
+export const ActionsData = {
+    onArchiveTask: fn(),
+    onPinTask: fn(),
+}
+
+const meta: Meta<TaskComponent> = {
+    title: 'Task',
+    component: TaskComponent,
+    excludeStories: /.*Data$/,
+    tags: ['autodocs'],
+    args: {...ActionsData}
+}
+export default meta;
+
+type Story = StoryObj<TaskComponent>;
+export const Default: Story = {
+    args: {
+        task: { id: '1', title: 'Test Task', state: 'TASK_INBOX' },
+    }
+}
+
+export const Pinned: Story = {
+    args: {
+        task: {...Default?.args?.task, state: 'TASK_PINNED'}
+    }
+}
+
+export const Archived: Story = {
+    args: {
+        task: {...Default?.args?.task, state: 'TASK_ARCHIVED'}
+    }
+}
